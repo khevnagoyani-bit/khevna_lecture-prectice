c7088926001f3288d7e2167d6928d34b0d3d7fae
khevnagoyani-bit <khevnagoyani@gmail.com>
2026-05-15 14:09:17 +0530

    Add files via upload

---
+++ 13-5-2026/demo.py.py
@@ -0,0 +1,55 @@
+# Python Basics
+
+# Function : Bulit - Functions(system function) \ UDF (user defind function)
+
+'''print()
+input()
+id()
+len()
+type()
+ect.....
+'''
+
+print("Hello , My Name Is Khevna.")
+
+# Add Two Numbers
+
+a = 10
+b = 20
+
+sum = a + b
+
+print(sum)
+
+# Input()
+
+name = input ("what is your name:")
+
+print("Welcome To Python Programming ", name)
+
+print(type(name))
+
+# Simple Calcutor
+
+a = int(input("Enter First Number:"))
+
+b = int(input("Enter second Number:"))
+
+print("Addition: ", a + b)
+
+print("substaction: ", a - b)
+
+print("multiply: " ,a * b)
+
+print("division: " ,a / b)
+
+# Silly Sandwich Maker
+
+bread = input("chooes ypur bread(white / brown):")
+
+filling = input("chooes ypur filling(cheese | jelly | banana)")
+
+print("here is silly sandwich:")
+
+print(" [" + bread + "bread+ " + filling + " + rainbow glitter]")
+
